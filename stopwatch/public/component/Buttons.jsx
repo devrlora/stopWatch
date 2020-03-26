98c5a3d225a37d38fc533671fb70e048f3de819e
Student_GA113 <Student_GA113@Student-GA113.local>
2020-03-25 22:13:58 -0400

    added files

--- stopwatch/public/component/Buttons.jsx
+++ stopwatch/public/component/Buttons.jsx
@@ -1,17 +1,55 @@
-import React from "react";
+// import React from "react";
 
-const Buttons = React.createclass({
+// const Buttons = React.createclass({
 
-    render:function(){
-        return <div>
-<h1>0:00</h1>
-<button type="button">start</button>
-</div>;
-}
-});
+//     render:function(){
+//         return <div>
+// <h1>0:00</h1>
+// <button type="button">start</button>
+// </div>;
+// }
+// });
 
-React.render({Buttons},document.body);
- export default Buttons;
+// React.render({Buttons},document.body);
+//  export default Buttons;
 
-
+class TimerInput extends React.Component {
+    render() {
+      return (
+       <div style={{marginLeft:100}}>
+          <h3>Input your desired time</h3>
+          <input type="number" required />
+      </div>
+         );
+       }
+    }
+    class Timer extends React.Component {
+       render() {
+          return (
+           <div>
+            <h1 style={{ fontSize: 100, marginLeft:100 }}> </h1>
+           </div>
+         );
+       }
+     }
+    class StartButton extends React.Component {
+       render() {
+         return(
+          <div style={{ marginLeft: 130 }}>
+           <button>Start</button>
+         </div>
+          );
+      }
+    }
+    class App extends React.Component {
+       render() {
+          return (
+           <div>
+            <TimerInput/>
+            <Timer/>
+           <StartButton/>
+          </div>
+         );
+       }
+     }
 
