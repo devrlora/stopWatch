8cb8c69eb9269ab855f4a1dc11581c26ab5b6bb3
Student_GA113 <Student_GA113@Student-GA113.local>
2020-03-25 21:03:54 -0400

    create buttons

---
+++ stopwatch/public/component/Buttons.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Buttons= React.createclass({
+
+    render:function(){
+        return <div>
+<h1>0:00</h1>
+<button type="button">start</button>
+</div>;
+}
+});
+
+React.render(<Buttons/>,document.body);
+// export default Buttons;
+
