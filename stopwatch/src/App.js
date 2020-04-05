ef52ef9af3e4857b29581aebb5fdf03e2cb571a6
Student_GA113 <Student_GA113@Student-GA113.local>
2020-04-05 02:43:30 -0400

    fixed path error

--- stopwatch/src/App.js
+++ stopwatch/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
-import Buttons from './Buttons';
+import Timer from './Timer';
 
 function App() {
  
   return (
     <div className="App">
-      <Buttons />
+      <Timer />
     </div>
   );
   }
