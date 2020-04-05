7c13c543d9d979ef0fda718ae464ae762cf468d2
Student_GA113 <Student_GA113@Student-GA113.local>
2020-04-05 00:57:54 -0400

    delete component buttons

--- stopwatch/src/App.js
+++ stopwatch/src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
+// import ReactDOM from 'react-dom';
 import './App.css';
+import Buttons from './Buttons';
 
 function App() {
+ 
   return (
     <div className="App">
-      <p>Hello World</p>
-      
+      <p>Display time here</p>
+      <Buttons/>
     </div>
   );
-}
+  }
 
 export default App;
