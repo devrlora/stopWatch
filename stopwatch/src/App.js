7d32b5ae285a7326eccf66d9920b845e1e49595b
Student_GA113 <Student_GA113@Student-GA113.local>
2020-04-05 01:08:32 -0400

    remove <p> element from file

--- stopwatch/src/App.js
+++ stopwatch/src/App.js
@@ -6,7 +6,6 @@ function App() {
  
   return (
     <div className="App">
-      <p>Display time here</p>
       <Buttons />
     </div>
   );
--- stopwatch/src/Buttons.jsx
+++ stopwatch/src/Buttons.jsx
@@ -2,17 +2,21 @@ import React from "react";
 import ReactDOM from 'react-dom';
 
 /*create a Button class the listens for an onClick event, starts/pauses timer,*/
-export class Buttons extends React.Component {
+class Buttons extends React.Component {
     render(){
 
         /* boolean statement that checks if button was started or not*/
 
         return ( 
         /* return new state of button*/
-
+        <div>
+        <button>Start</button>
+        <button>Stop</button>
+        <button>Reset</button>
+        </div>
+       
         );
     }
 }
         /* pass Buttons component to App*/
-ReactDOM.render(<Buttons />,document.getElementById('app'));
-
+export default Buttons;
